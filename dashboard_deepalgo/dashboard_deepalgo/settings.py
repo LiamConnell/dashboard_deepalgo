98e25d081ced9d5a156de75c5606dd8b62fd1d66
Liam Connell <ljrconnell@gmail.com>
2016-07-20 10:16:08 -0300

    modified settings, need unix for psql

--- dashboard_deepalgo/dashboard_deepalgo/settings.py
+++ dashboard_deepalgo/dashboard_deepalgo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backtests.apps.BacktestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
